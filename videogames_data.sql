6c87b06fd43cb64397b881e2c5523cd9c1f397d3
BelloDev <fernandojbf123@gmail.com>
2025-08-15 18:06:11 -0600

    add: Day2. Activities

---
+++ videogames_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO desarrolladores (Nombre, Pais) VALUES
+('Naughty Dog','Estados Unidos'),
+('Nintendo','Japón'),
+('CD Projekt Red','Polonia');
+
+INSERT INTO Plataformas (Nombre, Tipo) VALUES
+('Playstation 4','Consola'),
+('Nintendo Switch','Consola'),
+('PC','Computadora');
+
+INSERT INTO juegos (Titulo,AnoLanzamiento,DesarrolladorID,PlataformaID) VALUES
+('The last of us Part II',2020,1,1),
+('The legend of Zelda: Breath of the wild',2017,2,2),
+('Cyberpunk 2077',2020,3,3);
